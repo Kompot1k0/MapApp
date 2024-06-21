c275785824685cf5b01c01b0a503fa619b28e0e1
Eduard Marrugo <marruguera@gmail.com>
2024-06-21 12:25:13 +0300

    [Feature] add properties to work with map

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,42 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
-    
+    // all downloaded locations
     @Published var locations: [Location]
+    
+    // current location
+    @Published var mapLocation: Location
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
+        let defaultLocation = Location(
+            name: "Colosseum",
+            cityName: "Rome",
+            coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+            description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever built, and is still the largest standing amphitheatre in the world today, despite its age.",
+            imageNames: [
+                "rome-colosseum-1",
+                "rome-colosseum-2",
+                "rome-colosseum-3",
+            ],
+            link: "https://en.wikipedia.org/wiki/Colosseum")
         self.locations = locations
+        self.mapLocation = locations.first ?? defaultLocation
+        self.updateMapRegion(location: locations.first ?? defaultLocation)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
     }
 }
