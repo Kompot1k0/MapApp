8485a77e3d17466c62737af6a0113c40a3182196
Eduard Marrugo <marruguera@gmail.com>
2024-06-21 13:17:47 +0300

    [Feature] add LocationListView

---
+++ MapApp/Views/LocationsListView.swift
@@ -0,0 +1,50 @@
+//
+//  LocationsListView.swift
+//  MapApp
+//
+//  Created by Admin on 21.06.2024.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    var body: some View {
+        List {
+            ForEach(vm.locations) { location in
+                listRow(for: location)
+            }
+        }
+    }
+}
+
+extension LocationsListView {
+    private func listRow(for location: Location) -> some View {
+        HStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(10)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(location.name)
+                    .font(.headline)
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
+struct LocationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsListView()
+            .environmentObject(LocationsViewModel())
+    }
+}
